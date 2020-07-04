ade919a52c010b32f7e253572fe4855c091114e6
zhangfei <1490427952@qq.com>
2020-07-04 17:20:30 +0800

    update project path

--- xf-web/src/main/java/com/map8/xf/XfApplication.java
+++ xf-web/src/main/java/com/map8/xf/XfApplication.java
@@ -1,13 +1,13 @@
-package com.map8.demo;
+package com.map8.xf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class XfApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(XfApplication.class, args);
 	}
 
 }
--- xf-web/src/test/java/com/map8/xf/XfApplicationTests.java
+++ xf-web/src/test/java/com/map8/xf/XfApplicationTests.java
@@ -1,10 +1,10 @@
-package com.map8.demo;
+package com.map8.xf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class XfApplicationTests {
 
 	@Test
 	void contextLoads() {
